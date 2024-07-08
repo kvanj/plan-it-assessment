a4f123aeb13b8b0edd1b0105d9b23abab55d1e0d
Neil Capule <N.Capule@racingvictoria.net.au>
2024-07-08 21:35:38 +0800

    Add list of available ids and selector

---
+++ cypress/support/id.js
@@ -0,0 +1,42 @@
+/** URL Routes **/
+export const Routes = {
+    HOME: '/home',
+    SHOP: '/shop',
+    CONTACT: '/contact',
+    CART: '/cart'
+}
+
+/** Common Page **/
+export const CommonKeys = {
+    NAV_HOME: 'nav-home',
+    NAV_SHOP: 'nav-shop',
+    NAV_CONTACT: 'nav-contact',
+    NAV_LOGIN: 'nav-login',
+    NAV_CART: 'nav-cart'
+}
+
+/** Contact Page **/
+export const ContactKeys = {
+    INPUT_FORENAME: 'forename',
+    INPUT_SURNAME: 'surname',
+    INPUT_EMAIL: 'email',
+    INPUT_TEL: 'telephone',
+    TEXTAREA_MESSAGE: 'message',
+    BTN_SUBMIT: '.btn-contact',
+    ERROR_ALERT: '.alert-error',
+    ERROR_FORENAME: 'forename-err',
+    ERROR_EMAIL: 'email-err',
+    ERROR_MESSAGE: 'message-err',
+    ALERT_SUCCESS: '.alert-success'
+}
+
+/** Shop Page **/
+export const ShopKeys = {
+    BTN_BUY: '.btn-success'
+}
+
+/** Cart Page **/
+export const CartKeys = {
+    INPUT_QUANTITY: '.ng-valid-number',
+    TOTAL: '.total'
+}
